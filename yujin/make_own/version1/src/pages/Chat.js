63df1706847460bc3d950a1f2c84f6fc69ef5aae
mong_head <camara1melon315@gmail.com>
2021-08-10 17:21:20 +0900

    #Cap.[FEAT]: chat version1

--- yujin/make_own/version1/src/pages/Chat.js
+++ yujin/make_own/version1/src/pages/Chat.js
@@ -1,7 +1,26 @@
+import React, {useState} from 'react';
 import '../assets/css/App.css';
 import App from "../App";
 
-function Chat() {
+function Chat(props) {
+    // query parameter
+    const querys = props.location.search.replace("?","").split('&');
+    const parameter = {};
+    querys.map(query => {
+        query = query.split('=');
+        parameter[query[0]] = query[1];
+    })
+
+    // form
+    const [msg, setMsg] = useState('');
+
+    const onChangeInputMsg = (e) => {
+        setMsg(e.target.value);
+    }
+
+    const formSubmit = (e) => {
+        console.log(e)
+    }
     return(
         <div className="chat-container">
               <header className="chat-header">
@@ -11,20 +30,21 @@ function Chat() {
               <main className="chat-main">
                 <div className="chat-sidebar">
                   <h3><i className="fas fa-comments"></i> Room Name:</h3>
-                  <h2 id="room-name"></h2>
+                  <h2 id="room-name">{parameter['room']}</h2>
                   <h3><i className="fas fa-users"></i> Users</h3>
-                  <ul id="users"></ul>
+                  <ul id="users">{parameter['username']}</ul>
                 </div>
                 <div className="chat-messages"></div>
               </main>
               <div className="chat-form-container">
-                <form id="chat-form">
+                <form id="chat-form" onSubmit={formSubmit}>
                   <input
                     id="msg"
                     type="text"
                     placeholder="Enter Message"
                     required
                     autocomplete="off"
+                    value={ msg } onChange={ onChangeInputMsg }
                   />
                   <button className="btn"><i className="fas fa-paper-plane"></i> Send</button>
                 </form>
