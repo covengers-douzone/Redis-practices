df555a50a3c957859fe6597534afa00b07d43c17
mong_head <camara1melon315@gmail.com>
2021-08-16 00:28:40 +0900

    #Cap.[REFACTOR]: vanila code -> use jQuery

--- yujin/make_own/chat-cord-mysql/public/js/main.js
+++ yujin/make_own/chat-cord-mysql/public/js/main.js
@@ -1,21 +1,21 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+$(function () {
+    // Get username and room from URL
+    const {username, room} = Qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
 
-// Get username and room from URL
-const {username, room} = Qs.parse(location.search, {
-    ignoreQueryPrefix: true
-});
+    const chatForm = $('#chat-form');
+    const roomName = $('#room-name');
+    const userList = $('#users');
+    const chatMessages = $('.chat-messages');
 
-$(function(){
     // 방에 처음 들어왔을 때
-    // update status
+    // update status : true
     $.ajax({
         url: "/api/status",
         data: {
-            user: username,         // 나중에 변경해야할듯?
-            roomName: roomName.innerText,
+            userName: username,         // 나중에 변경해야할듯?
+            roomName: room,
             status: true
         },
         dataType: "json", // 받을 때 format
@@ -24,87 +24,87 @@ $(function(){
             console.log('success');
         }
     });
-})
 
-const socket = io();
+    const socket = io();
 
-const sendMessage = function (msg) {
-    $.ajax({
-        url: "/api/chat",
-        data: {
-            sender: username,         // 나중에 변경해야할듯?
-            message: msg,
-            roomName: roomName.innerText
-        },
-        dataType: "json", // 받을 때 format
-        type: "get", // 요청 method
-        success: function (response) {
-            console.log('success');
+    // Join chatroom
+    socket.emit('joinRoom', {username, room});
+
+    // Get room and users
+    socket.on('roomUsers', ({room, users}) => {
+        outputRoomName(room);
+        outputUsers(users);
+    });
+
+    // Message from server
+    socket.on('message', (message) => {
+        outputMessage(message);
+
+        // Scroll down
+        chatMessages.scrollTop(chatMessages.prop('scrollHeight'));
+    });
+
+    // Message submit
+    chatForm.submit((event) => {
+        event.preventDefault();
+
+        // Get message text
+        const msg = event.target.elements.msg.value;
+        sendMessage(msg);
+
+        // Clear input
+        event.target.elements.msg.value = '';
+        event.target.elements.msg.focus();
+    });
+
+    //Prompt the user before leave chat room
+    $('#leave-btn').click(() => {
+        const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+        if (leaveRoom) {
+            window.location = '../index.html';
+        } else {
         }
     });
-}
 
-// Join chatroom
-socket.emit('joinRoom', {username, room});
+    /* FUNCTIONs */
 
-// Get room and users
-socket.on('roomUsers', ({room, users}) => {
-    outputRoomName(room);
-    outputUsers(users);
-});
-
-
-// Message from server
-socket.on('message', (message) => {
-    outputMessage(message);
-
-    // Scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-// Message submit
-chatForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    // Get message text
-    const msg = event.target.elements.msg.value;
-    sendMessage(msg);
-
-    // Clear input
-    event.target.elements.msg.value = '';
-    event.target.elements.msg.focus();
-});
-
-// output message to DOM
-function outputMessage(message) {
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `
-  <p class="meta">${message.username}<span>${message.time}</span></p>
-  <p class="text">
-    ${message.text}
-  </p>
-  `;
-    document.querySelector('.chat-messages').appendChild(div);
-}
-
-// Add room name to DOM 
-function outputRoomName(room) {
-    roomName.innerText = room;
-}
-
-// Add users to DOM
-function outputUsers(users) {
-    userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join('')}
-  `;
-}
-
-//Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-    const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-    if (leaveRoom) {
-        window.location = '../index.html';
-    } else {
+    // call sendMessage api
+    const sendMessage = function (msg) {
+        $.ajax({
+            url: "/api/chat",
+            data: {
+                sender: username,         // 나중에 변경해야할듯?
+                message: msg,
+                roomName: room
+            },
+            dataType: "json", // 받을 때 format
+            type: "get", // 요청 method
+            success: function (response) {
+                console.log('success');
+            }
+        });
     }
-});
+
+    // output message to DOM
+    const outputMessage = (message) => {
+        const div = document.createElement('div');
+        div.classList.add('message');
+        div.innerHTML = `
+          <p class="meta">${message.username}<span>${message.time}</span></p>
+          <p class="text">
+            ${message.text}
+          </p>
+        `;
+        document.querySelector('.chat-messages').appendChild(div);
+    }
+
+    // Add room name to DOM
+    const outputRoomName = (room) => {
+        roomName.text(room);
+    }
+
+    // Add users to DOM
+    const outputUsers = (users) => {
+        userList.append( `${users.map(user => `<li>${user.username}</li>`).join('')}`);
+    }
+})
